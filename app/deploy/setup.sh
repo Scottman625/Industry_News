9603de87b9c73a22dc24c4e5e0020b9a993a45a9
Scottman625 <scottman608@gmail.com>
2025-03-06 13:55:27 +0800

    update setup.sh

--- app/deploy/setup.sh
+++ app/deploy/setup.sh
@@ -6,43 +6,60 @@ set -e
 PROJECT_GIT_URL='https://github.com/Scottman625/Industry_News.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/industry_news'
-
 PROJECT_PATH='/usr/local/apps/industry_news/app'
 
 echo "Installing dependencies..."
 apt-get update
-echo "Hello..."
 apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
-# Create project directorycd
-mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+# 處理已存在的目錄
+if [ -d "$PROJECT_BASE_PATH" ]; then
+    echo "Directory $PROJECT_BASE_PATH already exists"
+    echo "Updating existing repository..."
+    cd $PROJECT_BASE_PATH
+    git fetch
+    git reset --hard origin/main
+else
+    echo "Creating project directory..."
+    mkdir -p $PROJECT_BASE_PATH
+    git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+fi
 
-# Create virtual environment
-mkdir -p $PROJECT_BASE_PATH/env
-python3 -m venv $PROJECT_BASE_PATH/env
+# 處理虛擬環境
+if [ -d "$PROJECT_BASE_PATH/env" ]; then
+    echo "Virtual environment already exists"
+else
+    echo "Creating virtual environment..."
+    mkdir -p $PROJECT_BASE_PATH/env
+    python3 -m venv $PROJECT_BASE_PATH/env
+fi
 
-apt-get install python3-pip
+# 更新 pip
+$PROJECT_BASE_PATH/env/bin/python3 -m pip install --upgrade pip
 
-# Install python packages
+# 安裝 Python 套件
+echo "Installing Python packages..."
 $PROJECT_BASE_PATH/env/bin/python3 -m pip install -r $PROJECT_BASE_PATH/requirements.txt
 $PROJECT_BASE_PATH/env/bin/python3 -m pip install uwsgi
 
-# Run migrations and collectstatic
+# 執行資料庫遷移和收集靜態文件
+echo "Running migrations and collecting static files..."
 cd $PROJECT_PATH
 $PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
 
-# Configure supervisor
+# 配置 supervisor
+echo "Configuring supervisor..."
 cp $PROJECT_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Configure nginx
+# 配置 nginx
+echo "Configuring nginx..."
 cp $PROJECT_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+rm -f /etc/nginx/sites-enabled/default
+ln -sf /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
-echo "DONE! :)"
+echo "Deployment completed successfully! :)"
