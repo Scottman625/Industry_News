b7eebde1c2ea7951e08af644f99594888a6530a6
Scottman625 <scottman608@gmail.com>
2025-03-06 21:56:03 +0800

    add admin

--- app/modelCore/admin.py
+++ app/modelCore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Industry, Keyword, NewsArticle
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Industry)
+admin.site.register(Keyword)
+admin.site.register(NewsArticle)
+
+
+
